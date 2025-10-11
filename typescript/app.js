eab5e12ef1250ca3e616db7b2412eaa2ddbc7381
nikhilprashar561 <nikhilprashar561@gmail.com>
2025-10-11 23:42:02 +0530

    today end

--- typescript/app.js
+++ typescript/app.js
@@ -1,32 +1,16 @@
 "use strict";
-// let arr : number[] = [1,2,3,4,5,6]
+// // let arr : number[] = [1,2,3,4,5,6]
 Object.defineProperty(exports, "__esModule", { value: true });
-// let arr : [string, number, ...string[]];
-// arr = ["kaka", 44, "Hello", "ghs"]
-var userRole;
-(function (userRole) {
-    userRole["admin"] = "ADMIN";
-    userRole["normal"] = "NORMAL";
-    userRole["editor"] = "EDITOR";
-})(userRole || (userRole = {}));
-function checkAccess(role) {
-    if (role === userRole.admin) {
-        console.log(`WOW I'm So Happy You're ${userRole.admin}`);
-    }
-    else if (role === userRole.normal) {
-        console.log(`WOW I'm not Happy You're ${userRole.normal} My Advise You Must Be Upgrade Your Self`);
-    }
-    else {
-        console.log(`You're ${userRole.editor} You have a full Access to do anything what you want`);
-    }
+function check(obj, oj) {
+    console.log(obj.name);
+    console.log(oj.mobile);
+    // console.log(oj.password)
 }
-checkAccess(userRole.editor);
-// console.log(userRole)
-let a = 10; // When typeScript automatically fetch vairable which data type of value user store this is called "Type Infrence".
-// Below Type annotations 👇 :- When user define Which data type of value they are store in variable.
-let b;
-function check(a, b) {
-    console.log(`First value of ${a} and then second ${b}`);
-}
-check(28, "May");
+check({ name: "Nikhil" }, { mobile: 6239130326 });
+const obj1 = {
+    age: 25,
+    username: "Nikhil",
+    isVerified: false,
+    // email:"sj" if value is not define in interface here you cannot add any aditional value here.
+};
 //# sourceMappingURL=app.js.map
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,38 +1,68 @@
-// let arr : number[] = [1,2,3,4,5,6]
+// // let arr : number[] = [1,2,3,4,5,6]
 
-// let arr : [string, number, ...string[]];
+// // let arr : [string, number, ...string[]];
 
-// arr = ["kaka", 44, "Hello", "ghs"]
+// // arr = ["kaka", 44, "Hello", "ghs"]
 
-enum userRole {
-  admin = "ADMIN",
-  normal = "NORMAL",
-  editor = "EDITOR",
+// enum userRole {
+//   admin = "ADMIN",
+//   normal = "NORMAL",
+//   editor = "EDITOR",
+// }
+
+// function checkAccess(role: userRole) {
+//   if (role === userRole.admin) {
+//     console.log(`WOW I'm So Happy You're ${userRole.admin}`);
+//   } else if (role === userRole.normal) {
+//     console.log(
+//       `WOW I'm not Happy You're ${userRole.normal} My Advise You Must Be Upgrade Your Self`
+//     );
+//   } else {
+//     console.log(
+//       `You're ${userRole.editor} You have a full Access to do anything what you want`
+//     );
+//   }
+// }
+// checkAccess(userRole.editor)
+
+// // console.log(userRole)
+
+// let a = 10; // When typeScript automatically fetch vairable which data type of value user store this is called "Type Infrence".
+
+// // Below Type annotations 👇 :- When user define Which data type of value they are store in variable.
+
+// let b: number | boolean;
+// function check(a: number, b: string){
+//   console.log(`First value of ${a} and then second ${b}`)
+// }
+// check(28, "May")
+
+// // Below interface and types 👇
+
+interface user {
+  name?: String,
+  mobile?:Number,
+  email?:String
+}
+interface Admin extends user{
+  password?: String
 }
 
-function checkAccess(role: userRole) {
-  if (role === userRole.admin) {
-    console.log(`WOW I'm So Happy You're ${userRole.admin}`);
-  } else if (role === userRole.normal) {
-    console.log(
-      `WOW I'm not Happy You're ${userRole.normal} My Advise You Must Be Upgrade Your Self`
-    );
-  } else {
-    console.log(
-      `You're ${userRole.editor} You have a full Access to do anything what you want`
-    );
-  }
+function check(obj: user, oj: Admin){
+  console.log(obj.name)
+  console.log(oj.mobile)
+  // console.log(oj.password)
 }
-checkAccess(userRole.editor)
+check({name:"Nikhil"}, {mobile:6239130326})
 
-// console.log(userRole)
-
-let a = 10; // When typeScript automatically fetch vairable which data type of value user store this is called "Type Infrence".
-
-// Below Type annotations 👇 :- When user define Which data type of value they are store in variable.
-
-let b: number | boolean;
-function check(a: number, b: string){
-  console.log(`First value of ${a} and then second ${b}`)
+interface obj{
+  username:String,
+  age:Number,
+  isVerified:boolean
 }
-check(28, "May")
+const obj1 : obj = {
+  age: 25,
+  username:"Nikhil",
+  isVerified: false,
+  // email:"sj" if value is not define in interface here you cannot add any aditional value here.
+}
