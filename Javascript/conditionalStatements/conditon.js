d5ecd19646dd0e9d29351c4578911129ea65e1ea
nikhilprashar561 <nikhilprashar561@gmail.com>
2026-02-09 01:27:24 +0530

    Day 02 in JS

--- Javascript/conditionalStatements/conditon.js
+++ Javascript/conditionalStatements/conditon.js
@@ -31,3 +31,61 @@ function cinemaPricing(age, weekend) {
 }
 
 cinemaPricing(2, true);
+
+function getTrafficAction(color) {
+  color = color.toLowerCase()
+  let action;
+  switch (color) {
+    case "green":
+      action = "GO";
+      break;
+    case "yellow":
+      action = "SLOW DOWN";
+      break;
+    case "red":
+      action = "STOP";
+      break;
+    case "flashing red":
+      action = "STOP AND PROCEED WITH CAUTION";
+      break;
+    default:
+      action = "INVALID SIGNAL";
+      break;
+  }
+  return action;
+}
+
+getTrafficAction("Red")
+
+function calculateGrade(score, hasExtraCredit) {
+  let grade;
+  if (score < 0 || score > 100) {
+    return "INVALID";
+  }
+  if (hasExtraCredit) {
+    score = score + 5;
+    if(score > 100){
+      score = 100
+    }
+  }
+
+  if(score >= 90 && score <= 100){
+    grade = "A"
+  }
+  if(score >= 80 && score <= 89){
+    grade = "B"
+  }
+  if(score >= 70 && score <= 79){
+    grade = "C"
+  }
+  if(score >= 60 && score <= 69){
+    grade = "D"
+  }
+  if(score >= 0 && score <= 59){
+    grade = "F"
+  }
+
+  return grade;
+}
+
+calculateGrade(24, false)
